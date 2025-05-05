9ddefb077e24f2bee5945e9669ac426512d6a37e
Piyapat <piyapat.thongrueng@gmail.com>
2025-05-05 15:04:24 +0700

    Create project and contect with mongoDB

---
+++ springboot-webflux-project/src/main/java/com/piyapatproject/webflux/SpringbootWebfluxProjectApplication.java
@@ -0,0 +1,13 @@
+package com.piyapatproject.webflux;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootWebfluxProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootWebfluxProjectApplication.class, args);
+	}
+
+}
---
+++ springboot-webflux-project/src/test/java/com/piyapatproject/webflux/SpringbootWebfluxProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.piyapatproject.webflux;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootWebfluxProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
