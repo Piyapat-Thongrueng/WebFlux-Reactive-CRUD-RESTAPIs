9c274dadad8317c3daa57b953cde5a477ce5ffb4
Piyapat <piyapat.thongrueng@gmail.com>
2025-05-06 09:02:21 +0700

    Write an Integration test for Add Employee REST API

---
+++ springboot-webflux-project/src/test/java/com/piyapatproject/webflux/EmployeeControllerIntegrationTests.java
@@ -0,0 +1,41 @@
+package com.piyapatproject.webflux;
+
+
+import com.piyapatproject.webflux.dto.EmployeeDto;
+import com.piyapatproject.webflux.service.EmployeeService;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.reactive.server.WebTestClient;
+import reactor.core.publisher.Mono;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class EmployeeControllerIntegrationTests {
+
+    @Autowired
+    private EmployeeService employeeService;
+
+    @Autowired
+    private WebTestClient webTestClient;
+
+    @Test
+    public void testSaveEmployee() {
+        EmployeeDto employeeDto = new EmployeeDto();
+        employeeDto.setFirstName("John");
+        employeeDto.setLastName("Corner");
+        employeeDto.setEmail("john.corner@work.com");
+
+        webTestClient.post().uri("/api/employees")
+                .contentType(MediaType.APPLICATION_JSON)
+                .accept(MediaType.APPLICATION_JSON)
+                .body(Mono.just(employeeDto), EmployeeDto.class)
+                .exchange()
+                .expectStatus().isCreated()
+                .expectBody()
+                .consumeWith(System.out::println)
+                .jsonPath("$.firstName").isEqualTo(employeeDto.getFirstName())
+                .jsonPath("$.lastName").isEqualTo(employeeDto.getLastName())
+                .jsonPath("$.email").isEqualTo(employeeDto.getEmail());
+    }
+}
